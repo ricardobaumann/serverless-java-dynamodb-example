5fd6a4903711cefb483e1b6a2aacc7effe1b183c
Ricardo Baumann <gemini.richard@gmail.com>
2017-10-05 20:03:14 +0200

    it works

--- src/main/java/rating/handler/GetHandler.java
+++ src/main/java/rating/handler/GetHandler.java
@@ -1,43 +1,18 @@
 package rating.handler;
 
-import com.amazonaws.regions.Region;
-import com.amazonaws.regions.Regions;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
-import com.amazonaws.services.dynamodbv2.model.AttributeValue;
-import com.amazonaws.services.dynamodbv2.model.ComparisonOperator;
-import com.amazonaws.services.dynamodbv2.model.Condition;
-import com.amazonaws.services.dynamodbv2.model.ScanRequest;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import rating.model.ListResponse;
 import rating.model.Rating;
-import rating.util.Constants;
-import rating.util.DynamoDBHelper;
-import rating.util.Util;
 
-import java.util.HashMap;
-import java.util.List;
 
 public class GetHandler implements RequestHandler<Rating, ListResponse> {
 
     @Override
     public ListResponse handleRequest(Rating rating, Context context) {
-        AmazonDynamoDBClient dynamoDBClient = new AmazonDynamoDBClient().withRegion(Region.getRegion(Regions.US_EAST_1));
-        if (rating.getChargeStationId() == null) {
-            Util.throwMissingKeyException(Constants.CHARGE_STATION_ID);
-        }
-        Condition scanFilterCondition = new Condition()
-                .withComparisonOperator(ComparisonOperator.EQ.toString())
-                .withAttributeValueList(new AttributeValue().withS(rating.getChargeStationId()));
 
-        HashMap<String, Condition> conditions = new HashMap<>();
 
-        conditions.put(Constants.CHARGE_STATION_ID, scanFilterCondition);
-        ScanRequest scanRequest = new ScanRequest().withTableName(Constants.DB_NAME).withScanFilter(conditions);
-
-        List<Rating> result = DynamoDBHelper.convertResultToRating(dynamoDBClient.scan(scanRequest));
-
-        return new ListResponse("GET result for chargeStationId: " + rating.getChargeStationId(), result);
+        return new ListResponse("GET result for chargeStationId: " + rating.getChargeStationId(), null);
     }
 
 }
--- src/main/java/rating/model/Response.java
+++ src/main/java/rating/model/Response.java
@@ -1,15 +1,50 @@
 package rating.model;
 
-import lombok.Data;
 
-@Data
-public class Response {
+import java.util.HashMap;
+import java.util.Map;
 
-    private String message;
-    private Rating rating;
 
-    public Response(String message, Rating rating) {
-        this.message = message;
-        this.rating = rating;
+import java.util.Collections;
+
+/**
+ * The response that will be the passed back to the API Gateway.
+ * <p>
+ * The implementation depends on the AWS API Gateway response template and
+ * is designed to get serialized to it.
+ *
+ * @author Bjoern Bilger
+ *
+ */
+public final class Response {
+
+    private final String body;
+    private final Map<String, String> headers;
+    private final int statusCode;
+    private final boolean base64Encoded;
+
+    public Response(String body,  Map<String, String> headers,
+                           int statusCode, boolean base64Encoded) {
+        this.statusCode = statusCode;
+        this.body = body;
+        this.headers = Collections.unmodifiableMap(new HashMap<>(headers));
+        this.base64Encoded = base64Encoded;
     }
+
+    public String getBody() {
+        return body;
+    }
+
+    public Map<String, String> getHeaders() {
+        return headers;
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+    // APIGW expects the property to be called "isBase64Encoded"
+    public boolean isIsBase64Encoded() {
+        return base64Encoded;
+    }
+
 }
